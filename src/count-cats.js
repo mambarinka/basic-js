14f63b7ce94c1c5912ad09bde8de912cb87dc6b7
Anna <mambarinka@mail.ru>
2021-02-08 17:29:58 +0400

    feat: implements Fn of counts cat

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,34 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  // function countCats(matrix) {
+  let numberCats = 0;
+
+  matrix.forEach(array => {
+    array.forEach(element => {
+      if (element === '^^') {
+        numberCats++;
+      }
+    });
+  });
+  console.log(numberCats);
+  return numberCats;
 };
+
+// function countCats(matrix) {
+//   let result = 0;
+//   matrix.forEach(element => {
+//     element.forEach(depthEl => {
+//       if (depthEl == "^^") {
+//         result++;
+//       }
+//     })
+//   });
+//   return result;
+// };
+
+// let a = countCats([
+//   ['##', 'dd', '00'],
+//   [' ^^ ', '..', 'ss'],
+//   ['AA', 'dd', 'Oo'],
+// ]);
