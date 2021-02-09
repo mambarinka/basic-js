c8bb53707298e0f1ca5debfa2b8e37ffcbbe9b0a
Anna <mambarinka@mail.ru>
2021-02-09 13:19:31 +0400

    refactor: formatting and delets consoles

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,24 +3,17 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
 
-// module.exports = function dateSample(sampleActivity) {
-  function dateSample(sampleActivity) {
+module.exports = function dateSample(sampleActivity) {
   let ageExcavation = 0;
   let ln2 = 0.693;
   let speedReaction = ln2 / HALF_LIFE_PERIOD;
 
-  if (typeof sampleActivity === 'string' && (parseInt(sampleActivity))) {
-    console.log('da');
-    console.log(parseInt(sampleActivity));
+  if (typeof sampleActivity === 'string' &&
+    (parseInt(sampleActivity)) &&
+    (sampleActivity > 0 && sampleActivity <= 15)) {
+
     ageExcavation = Math.ceil(Math.log(MODERN_ACTIVITY / Number(sampleActivity)) / speedReaction);
-    console.log(Math.log(MODERN_ACTIVITY / sampleActivity));
-    console.log(speedReaction);
-    console.log(Math.log(MODERN_ACTIVITY / sampleActivity) / speedReaction);
+
   }
-  console.log(typeof ageExcavation);
-  console.log(ageExcavation);
   return ageExcavation;
 }
-
-
-dateSample('9000');
