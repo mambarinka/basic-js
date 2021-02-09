64ed954673cb3184991c33305828741cf2b0fc22
Anna <mambarinka@mail.ru>
2021-02-09 16:45:21 +0400

    feat: finishs to implement Fn of name team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,51 +1,22 @@
 const CustomError = require("../extensions/custom-error");
 
-// module.exports = function createDreamTeam(members) {
-function createDreamTeam(members) {
+module.exports = function createDreamTeam(members) {
   let nameDreamTeam = [];
   if (members == undefined || members == null || members == false) {
     return false;
   }
 
   for (let i = 0; i < members.length; i++) {
-    if (typeof members[i] !== 'string' || members[i] === null || members[i] === undefined) {
-      return false;
+    if (typeof members[i] !== 'string' ||
+      members[i] === null ||
+      members[i] === undefined ||
+      typeof members[i] === 'boolean') {
+      continue;
     } else {
       members[i] = members[i].replace(/\s+/g, '').toUpperCase();
       nameDreamTeam.push(members[i][0]);
     }
   }
-  console.log(typeof nameDreamTeam);
-  console.log(nameDreamTeam.sort().join(''));
-  console.log(typeof nameDreamTeam);
+
   return nameDreamTeam.sort().join('');
 }
-
-createDreamTeam([
-  'Amelia',
-  null,
-  undefined,
-  'Ruby',
-  'Lily',
-  11,
-  'Grace',
-  22,
-  'Millie',
-  'Daisy',
-  true,
-  'Freya',
-  false,
-  'Erin',
-  new Set([1, 2, 3, 4, 5]),
-  'Megan',
-  {
-    'John': 'Smith'
-  },
-  'Jasmine',
-  1,
-  2,
-  3,
-  4,
-  5,
-  'Brooke',
-])
