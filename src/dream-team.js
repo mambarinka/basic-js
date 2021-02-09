936ceb701ae369b6a01122a1adb69f1e2d4ddcbf
Anna <mambarinka@mail.ru>
2021-02-09 16:40:17 +0400

    feat: starts to implement Fn on name of team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,51 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+// module.exports = function createDreamTeam(members) {
+function createDreamTeam(members) {
+  let nameDreamTeam = [];
+  if (members == undefined || members == null || members == false) {
+    return false;
+  }
+
+  for (let i = 0; i < members.length; i++) {
+    if (typeof members[i] !== 'string' || members[i] === null || members[i] === undefined) {
+      return false;
+    } else {
+      members[i] = members[i].replace(/\s+/g, '').toUpperCase();
+      nameDreamTeam.push(members[i][0]);
+    }
+  }
+  console.log(typeof nameDreamTeam);
+  console.log(nameDreamTeam.sort().join(''));
+  console.log(typeof nameDreamTeam);
+  return nameDreamTeam.sort().join('');
+}
+
+createDreamTeam([
+  'Amelia',
+  null,
+  undefined,
+  'Ruby',
+  'Lily',
+  11,
+  'Grace',
+  22,
+  'Millie',
+  'Daisy',
+  true,
+  'Freya',
+  false,
+  'Erin',
+  new Set([1, 2, 3, 4, 5]),
+  'Megan',
+  {
+    'John': 'Smith'
+  },
+  'Jasmine',
+  1,
+  2,
+  3,
+  4,
+  5,
+  'Brooke',
+])
