486a704422aab4f2d91c1e6b200257eae248aa0c
Anna <mambarinka@mail.ru>
2021-02-09 13:14:55 +0400

    feat: starts to implement Fn on age of excavation

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+// module.exports = function dateSample(sampleActivity) {
+  function dateSample(sampleActivity) {
+  let ageExcavation = 0;
+  let ln2 = 0.693;
+  let speedReaction = ln2 / HALF_LIFE_PERIOD;
+
+  if (typeof sampleActivity === 'string' && (parseInt(sampleActivity))) {
+    console.log('da');
+    console.log(parseInt(sampleActivity));
+    ageExcavation = Math.ceil(Math.log(MODERN_ACTIVITY / Number(sampleActivity)) / speedReaction);
+    console.log(Math.log(MODERN_ACTIVITY / sampleActivity));
+    console.log(speedReaction);
+    console.log(Math.log(MODERN_ACTIVITY / sampleActivity) / speedReaction);
+  }
+  console.log(typeof ageExcavation);
+  console.log(ageExcavation);
+  return ageExcavation;
+}
+
+
+dateSample('9000');
