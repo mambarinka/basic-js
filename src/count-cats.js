69ef75e13531e4616a79f1cc94e177a3946f48c5
Anna <mambarinka@mail.ru>
2021-02-09 10:40:17 +0400

    feat: adds 2 additional function options

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,34 +1,53 @@
 const CustomError = require("../extensions/custom-error");
 
+
+// 1 вариант с использованием вложенного forEach
+// module.exports = function countCats(matrix) {
+//   let numberCats = 0;
+
+//   matrix.forEach(array => {
+//     array.forEach(element => {
+//       if (element === '^^') {
+//         numberCats++;
+//       }
+//     });
+//   });
+//   console.log(numberCats);
+//   return numberCats;
+// };
+
+// 2 вариант с использованием обычного цикла
+
+// module.exports = function countCats(matrix) {
+//   let numberCats = 0;
+
+//   for (let i = 0; i < matrix.length; i++) {
+//     const element = matrix[i];
+//     for (let j = 0; j < element.length; j++) {
+//       if (element[j] === '^^') {
+//         numberCats++;
+//       }
+
+//     }
+//   }
+//   console.log(numberCats);
+//   return numberCats;
+// };
+
+
+// 3 вариант с использованием filter
 module.exports = function countCats(matrix) {
-  // function countCats(matrix) {
-  let numberCats = 0;
+  let arrayCats = [];
+  let cat = "^^";
 
   matrix.forEach(array => {
-    array.forEach(element => {
-      if (element === '^^') {
-        numberCats++;
+    array.filter(function (element) {
+      if (element === cat) {
+        arrayCats.push(element);
       }
     });
   });
-  console.log(numberCats);
-  return numberCats;
+
+  console.log(arrayCats.length);
+  return arrayCats.length;
 };
-
-// function countCats(matrix) {
-//   let result = 0;
-//   matrix.forEach(element => {
-//     element.forEach(depthEl => {
-//       if (depthEl == "^^") {
-//         result++;
-//       }
-//     })
-//   });
-//   return result;
-// };
-
-// let a = countCats([
-//   ['##', 'dd', '00'],
-//   [' ^^ ', '..', 'ss'],
-//   ['AA', 'dd', 'Oo'],
-// ]);
