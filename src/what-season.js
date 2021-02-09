b82fa1b7afe072bfc05a852d7c693c0f4c5ea882
Anna <mambarinka@mail.ru>
2021-02-09 17:58:43 +0400

    feat: starts to implements Fn look for a season

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,44 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+// const getSeason = function (date) {
+
+  if (date == null || date == undefined || date == false) {
+    return 'Unable to determine the time of year!';
+  }
+  /* else if (Object.prototype.toString.call(date) !== '[object Date]') {
+    throw Error;
+} */
+
+  let month = date.getMonth();
+
+  switch (String(month)) {
+    case '12':
+    case '1':
+    case '2':
+      console.log('winter');
+      return 'winter';
+    case '3':
+    case '4':
+    case '5':
+      console.log('spring');
+      return 'spring';
+    case '6':
+    case '7':
+    case '8':
+      console.log('summer');
+      return 'summer';
+    case '9':
+    case '10':
+    case '11':
+      console.log('autumn');
+      return 'autumn';
+    default:
+      throw new Error('Error');
+  }
+
 };
+
+// getSeason(new Date(2150, 7, 21, 18, 36, 41, 841));
+// getSeason('1');
+
