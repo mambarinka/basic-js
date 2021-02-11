4cd5223b779a9377d26ae89698142fb5a21a576b
Anna <mambarinka@mail.ru>
2021-02-11 15:00:54 +0400

    feat: starts to implement Fn for manage sequences

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,43 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+// module.exports = function transform(arr) {
+function transform(arr) {
+
+  // const manageSequences = {
+  //   '--discard-next': function(array) {
+  // array.splice
+  //   },
+  //   '--discard-prev': '',
+  //   '--double-next': '',
+  //   '--double-prev': '',
+  // };
+  let newArr = arr.slice();
+  console.log(`newArr15: ${newArr}`);
+
+  for (let i = 0; i < newArr.length; i++) {
+    console.log(newArr[i]);
+    if (newArr[i] === '--discard-next') {
+      console.log('нашлась управляющая последовательсность discard-next');
+      newArr.splice([i], 2);
+    }
+
+    if (newArr[i] === '--discard-prev') {
+      console.log('нашлась управляющая последовательсность discard-prev');
+      newArr.splice([i - 1], 2);
+    }
+  }
+  console.log(`newArr22: ${newArr}`);
+  return newArr;
+
 };
+
+transform([1, 2, 3, '--discard-next', 4, 5, 6, '--discard-prev', 7, 8]);
+// transform([1, 2, 3, '--discard-prev', 4, 5]);
+
+// let massiveNEXT = [1, 2, 3, '--discard-next', 4, 5];
+// massiveNEXT.splice(3, 1, '555');
+// console.log(massiveNEXT);
+
+// let massivePREV = [1, 2, 3, '--discard-prev', 4, 5];
+// massivePREV.splice(3, 1);
+// console.log(massivePREV);
