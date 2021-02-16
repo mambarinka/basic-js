1c8535b2530c3ce4040d5181501393da5b6a2b09
Anna <mambarinka@mail.ru>
2021-02-16 15:59:16 +0400

    feat: starts to implements Fn repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+// module.exports = function repeater(str, options) {
+function repeater(str, options) {
+  let repeatStr = str.split(' ');
+  for (let i = 1; i < options.repeatTimes; i++) {
+    repeatStr.push(str,options.addition);
+    // repeatStr.push(options.addition);
+  }
+  console.log(repeatStr.join(options.separator));
+  return repeatStr.join(options.separator);
 };
-  
+
+repeater('STRING', {
+  repeatTimes: 3,
+  separator: '**',
+  addition: 'PLUS'
+});
