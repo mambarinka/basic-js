c4ca9f7c096f08a4fea2f4aa53f8bd9563126e24
Anna <mambarinka@mail.ru>
2021-02-17 18:48:13 +0400

    feat: starts to implement Fn Vigenere

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  }
+  encrypt(message, key) {
+if (!message || !key) {
+  throw new Error('parameter not passed');
+}
+  }
+  decrypt(message, key) {
+
   }
 }
 
 module.exports = VigenereCipheringMachine;
+
+const directMachine = new VigenereCipheringMachine();
+
+
+directMachine.encrypt('attack at dawn!', 'alphonse');
+// AEIHQX SX DLLU!
+
+directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse');
+// ATTACK AT DAWN!
