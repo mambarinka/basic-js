d55e5d8d22c315509a63752ab5add12bbcdfddfa
Anna <mambarinka@mail.ru>
2021-02-19 15:31:36 +0400

    refactor: optimization (from 2 Fn to one)

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,21 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  constructor() {
+  constructor(isDirectMachine) {
+    this.isDirectMachine = isDirectMachine;
     this.alphabet = ['a', 'b', 'c', 'd', 'e', 'f',
       'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
       'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
       'y', 'z'
     ];
-
-    // this.encrypt = this.encrypt.bind(this);
   }
-
-  // if (!VigenereCipheringMachine.encrypt) {
-  //   throw new Error("parameter not passed");
-  // }
-
-  encrypt(message, key) {
+  cypher(message, key, encOrDec) {
     let result = '';
     let symbol = '';
     let notEngArray = [];
@@ -46,7 +40,12 @@ class VigenereCipheringMachine {
         } else {
           let mesIndex = this.alphabet.indexOf(messageLowercase[i]);
           let keyIndex = this.alphabet.indexOf(keyResult[i]);
-          symbol = this.alphabet[(mesIndex + keyIndex) % this.alphabet.length]; //по формуле из WiKi
+
+          if (encOrDec === true) {
+            symbol = this.alphabet[(mesIndex + keyIndex) % this.alphabet.length]; //по формуле из WiKi
+          } else {
+            symbol = this.alphabet[(mesIndex + this.alphabet.length - keyIndex) % this.alphabet.length];
+          }
         }
 
         result += symbol;
@@ -58,46 +57,13 @@ class VigenereCipheringMachine {
   }
 
 
+  encrypt(message, key) {
+    this.cypher(message, key, true);
+  }
+
+
   decrypt(message, key) {
-    let result = '';
-    let symbol = '';
-    let notEngArray = [];
-    let messageLowercase = message.toLowerCase();
-    let keyLowerCase = key.toLowerCase();
-
-    if (!message || !key) {
-      throw new Error('parameter not passed');
-    } else {
-
-      for (let i = 0; i < messageLowercase.length; i++) {
-        if (!this.alphabet.includes(messageLowercase[i])) { // если какой-либо символ текста не содержит англ
-          notEngArray.push(messageLowercase[i]);
-        }
-      }
-
-      let keyResult = keyLowerCase.split('');
-
-      while (keyResult.length < (messageLowercase.length - notEngArray.length)) { // если ключ меньше по длине , чем текст
-        for (let i = 0; i < messageLowercase.length; i++) {
-          keyResult.push(keyResult[i]);
-        }
-      }
-
-      for (let i = 0; i < message.length; i++) {
-        if (!this.alphabet.includes(messageLowercase[i])) { // если символ не англ, то просто вставляй в результат
-          symbol = messageLowercase[i];
-          keyResult.splice(keyResult[i], 0, ' ');
-        } else {
-          let mesIndex = this.alphabet.indexOf(messageLowercase[i]);
-          let keyIndex = this.alphabet.indexOf(keyResult[i]);
-          symbol = this.alphabet[(mesIndex + this.alphabet.length - keyIndex) % this.alphabet.length]; //по формуле из WiKi
-        }
-
-        result += symbol;
-      }
-
-      return result.toUpperCase();
-    }
+    this.cypher(message, key, false);
   }
 }
 
@@ -107,3 +73,5 @@ module.exports = VigenereCipheringMachine;
 const directMachine = new VigenereCipheringMachine();
 directMachine.encrypt('Samelengthkey', 'Samelengthkey');
 // KAYIWIAMMOUIW
+
+directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse');
